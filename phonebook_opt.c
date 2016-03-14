e20e974d896e63e04a0156c42b265bffb1dc2e81
Ping-Lin <billy3962@hotmail.com>
2016-03-14 17:52:45 +0800

    reduce the struct size and use hash table to construct the data structure

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,45 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastname[], entry *pHead)
+entry *findName(char lastname[], entry **pHead)
 {
-    /* TODO: implement */
+    unsigned int val = hash(lastname);
+    entry *tmp = pHead[val];
+    while(tmp != NULL) {
+        if(strcasecmp(lastname, tmp->lastName) == 0)
+            return tmp;
+        tmp = tmp->pNext;
+    }
+
     return NULL;
 }
 
-entry *append(char lastName[], entry *e)
+entry **append(char lastName[], entry **e)
 {
-    return NULL;
+    /*
+     * append at beginning of the block
+     * */
+    unsigned int val = hash(lastName);
+    entry *remainder = e[val]->pNext;
+    entry *tmp = e[val];
+
+    tmp->pNext = (entry *) malloc(sizeof(entry));
+    tmp = tmp->pNext;
+    strcpy(tmp->lastName, lastName);
+    tmp->pNext = remainder;
+
+    return e;
 }
+
+unsigned int hash(char key[])
+{
+    unsigned int hashValue = 0;
+    while(*key != '\0') {
+        hashValue = (hashValue << 5) + hashValue + *key++;
+    }
+    return hashValue % TABLESIZE;
+}
+
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,12 +2,13 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
+#define TABLESIZE 1024
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+
+typedef struct __PHONE_BOOK_DETAIL {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,10 +18,17 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} detail;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    int hashIndex;
+    struct __PHONE_BOOK_DETAIL *details;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
-entry *findName(char lastname[], entry *pHead);
-entry *append(char lastName[], entry *e);
+entry *findName(char lastname[], entry **pHead);
+entry **append(char lastName[], entry **e);
+unsigned int hash(char key[]);
 
 #endif
